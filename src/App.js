0832f4ee14ba79a4d88916e7dfc2758916455c61
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:11:51 +0530

    Set up routing and authentication in App component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Dashboard from "./pages/Dashboard";
+import Inventory from "./pages/Inventory";
+import Sell from "./pages/Sell";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthContext";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/sell" element={<Sell />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
 
