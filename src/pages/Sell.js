d625a3021b145c5ce2f15189027d928fd59fd179
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:17:09 +0530

    Add Sell component for processing sales

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,1 +1,14 @@
+import React from "react";
+
+function Sell() {
+  return (
+    <div className="sell glass">
+      <h2>Sell Items</h2>
+      <p>Here you’ll create bills and process sales.</p>
+      <button className="btn-green">Sell Now</button>
+    </div>
+  );
+}
+
+export default Sell;
 
